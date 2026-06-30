76e9bc87888847c64198a36706a146dee3f04642
L0tzie <l0tzie@outlook.com>
2026-06-30 16:07:44 -0400

    Update CloudMoon.js

--- API/CloudMoon.js
+++ API/CloudMoon.js
@@ -2,12 +2,14 @@ async function getCloudMoonUrls() {
   try {
     const response = await fetch('CloudMoon.json');
     if (!response.ok) return [];
+    
     const data = await response.json();
     return data.CloudMoonAPIURLS || [];
   } catch {
     return [];
   }
 }
+
 (async () => {
   window.cloudMoonUrls = await getCloudMoonUrls();
 })();
